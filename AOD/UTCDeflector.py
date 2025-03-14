ea91f38a9bf7e7a691747d3f1cab1b6f8c208953
pasha-cement <kolesnik-pavel-fsr@yandex.ru>
2025-03-14 15:41:03 +0300

    незначительные изменения

--- AOD/UTCDeflector.py
+++ AOD/UTCDeflector.py
@@ -10,11 +10,11 @@ logging.basicConfig(level=logging.INFO)
 
 class DevReader:
     def __init__(self, filename: str) -> None:
-        self.__dev = pd.read_csv(filename, sep="\t")
-        self.__angle_interpolator = interpolate.CubicSpline(self.__dev["Frequency"], self.__dev["Angle"])
+        self.__dev = pd.read_csv(filename)
+        self.__angle_interpolator = interpolate.CubicSpline(self.__dev["Angle"], self.__dev["Frequency"])
         
     def get_freq_by_angle(self, angle: float) -> float:
-        return self.__angle_interpolator.solve(angle)[1]
+        return self.__angle_interpolator(angle)
     
     def get_ampl_by_freq(self, freq: float) -> float:
         return np.interp(freq, self.__dev["Frequency"], self.__dev["Amplitude"])
@@ -30,28 +30,29 @@ class Deflector:
         return buffer
     
     def __send_command(self, buffer: list[int]) -> None:
-            self.__serial.write(bytearray(buffer))
-            time.sleep(0.01)
-
+        self.__serial.write(bytearray(buffer))
+        time.sleep(0.01)
+    
     def calculate_crc8(self, data: list[int]) -> int:
         return libscrc.hacker8(bytes(data), poly=0x31, init=0xFF)
-
+    
     def start(self) -> None:
         preamp_on_command = self.__make_command(0xA2, [1])
         amp_on_command = self.__make_command(0xA3, [1])
         self.__send_command(preamp_on_command)
         self.__send_command(amp_on_command)
     
-    def set_freq(self, freq:float) -> None:
+    def set_freq(self, freq: float) -> None:
         try:
             freq_bytes = int(freq * 100).to_bytes(2, 'little')
             set_freq_command = self.__make_command(0xA5, list(freq_bytes))
             self.__send_command(set_freq_command)
-        except:
+        except Exception as e:
+            logging.error(e)
             self.stop()
             self.close()
-
-    def set_angle(self, angle:float) -> None:
+    
+    def set_angle(self, angle: float) -> None:
         freq = self.__dev_reader.get_freq_by_angle(angle)
         self.set_freq(freq)
     
@@ -60,7 +61,8 @@ class Deflector:
             ampl_bytes = int(ampl * 10).to_bytes(2, 'little')
             set_ampl_command = self.__make_command(0xA7, list(ampl_bytes))
             self.__send_command(set_ampl_command)
-        except:
+        except Exception as e:
+            logging.error(e)
             self.stop()
             self.close()    
             
@@ -82,4 +84,3 @@ if __name__ == "__main__":
         time.sleep(1)
     deflector.stop()
     deflector.close()
-
